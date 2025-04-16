6dd61be380b9d23c9dcde05c4f128b2e1bb70a99
mwgupta <meiligupta@gmail.com>
2025-04-16 12:04:18 -0400

    rename other files

---
+++ mortgage.py
@@ -0,0 +1,13 @@
+import streamlit as st               
+
+st.markdown("# Practical Applications of Machine Learning (PAML)")
+
+#############################################
+
+st.markdown("### PAML Final - Predicting Mortgage Propensity Scores Using Regression")
+
+#############################################
+
+st.markdown("Customer acquisition, and preventing drop-off during the application process, is a central challenge for digital-first companies. Newrez, the fourth-largest mortgage lender in the U.S., receives over 30 million annual website visits but converts only 0.15% of these visitors into loan originations. This project aims to increase that rate through targeted interventions powered by machine learning. Specifically, we propose using Lasso and Ridge regression to predict drop-off risk across different stages of the application funnel. Based on a user’s predicted likelihood of completing the process, different prompts will be deployed - ranging from targeted ads to calls from loan officers. We will evaluate both models using metrics such as conversion rate, click-through rate (CTR), and application abandonment rate, and will deploy the highest-performing model in a Streamlit application. Our expected outcome is a doubling of the acquisition rate—from 0.15% to 0.30%—after implementation. More broadly, this work contributes a scalable framework for intelligent engagement in high-stakes online applications, with implications for banking, insurance, and other digital services.")
+
+st.markdown("Click **Explore Dataset** to get started.")
